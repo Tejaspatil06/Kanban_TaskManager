3c4a32d54b08aef5cc8d3f24ae9f6a29abc7ddc2
Tejas Patil <tejaspatil060105@gmail.com>
2024-09-26 12:32:45 +0530

    backend deploy

--- client/app/Components/Sidebar/Sidebar.tsx
+++ client/app/Components/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import { useUserContext } from "@/context/userContext";
 function Sidebar() {
   const { logoutUser } = useUserContext();
   return (
-    <div className="w-[20rem] mt-[5rem] h-[calc(100%-5rem)] fixed right-0 top-0 bg-[#f9f9f9] flex flex-col">
+    <div className="w-[20rem] mt-[5rem] h-[calc(100%-5rem)] fixed right-0 top-0 bg-[#0B051D] flex flex-col">
       <Profile />
       <button
         className="mt-auto mb-6 mx-6 py-4 px-8 bg-[#EB4E31] text-white rounded-[50px] hover:bg-[#3aafae] transition duration-200 ease-in-out"
