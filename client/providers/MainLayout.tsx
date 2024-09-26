2bdd6199aeec6aec546cee73765f74a00e9cb08d
Tejas Patil <tejaspatil060105@gmail.com>
2024-09-26 11:31:16 +0530

    frontend and backend

---
+++ client/providers/MainLayout.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Modal from "@/app/Components/Modal/Modal";
+import { useTasks } from "@/context/taskContext";
+import React from "react";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+function MainLayout({ children }: MainLayoutProps) {
+  const { isEditing, profileModal } = useTasks();
+  return (
+    <div className="main-layout flex-1 bg-[#EDEDED] border-2 border-white rounded-[1.5rem] overflow-auto">
+      {isEditing && <Modal />}
+      {children}
+    </div>
+  );
+}
+
+export default MainLayout;
